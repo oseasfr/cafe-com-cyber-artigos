49dcb9f2f06aa93d926c336f70368ab30f6e362b
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-02-05 22:03:22 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { Toaster } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
@@ -7,29 +5,26 @@ import ArticlePage from "./pages/articles/ArticlePage";
 import ArticlesFiles from "./pages/articles/ArticlesFiles";
 import NotFound from "./pages/NotFound";
 
-// Configuração do cliente para o React Query
+// Configuração simplificada do QueryClient
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <BrowserRouter basename="/cafe-com-cyber-artigos">
-        <Routes>
-          {/* Rota Principal: Home */}
-          <Route path="/" element={<Index />} />
-          
-          {/* Rota para Listagem de Artigos */}
-          <Route path="/articles" element={<ArticlesFiles />} />
-          
-          {/* Rota para Artigo Individual */}
-          <Route path="/articles/:id" element={<ArticlePage />} />
-          
-          {/* Rota para páginas não encontradas */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
+    <BrowserRouter basename="/cafe-com-cyber-artigos">
+      <Routes>
+        {/* Rota Principal */}
+        <Route path="/" element={<Index />} />
+        
+        {/* Listagem de Artigos */}
+        <Route path="/articles" element={<ArticlesFiles />} />
+        
+        {/* Página do Artigo */}
+        <Route path="/articles/:id" element={<ArticlePage />} />
+        
+        {/* Página 404 */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </QueryClientProvider>
 );
 

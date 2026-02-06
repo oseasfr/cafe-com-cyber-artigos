cc66beb78390753107e2a8ba65d92a6121ac8d1b
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-02-05 22:18:08 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,17 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { plugin as markdown } from "vite-plugin-markdown"; // Adicione isso
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    base: "/cafe-com-cyber-artigos/", 
-    
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          }
-        }
-      }
+export default defineConfig({
+  base: "/cafe-com-cyber-artigos/",
+  plugins: [
+    react(),
+    markdown({ mode: ["html", "react", "meta"] }) // Ativa o suporte a Markdown
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-
-    server: {
-      host: "::",
-      port: 8080,
-    },
-    plugins: [
-      react(),
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-      extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json'],
-    },
-  };
+  },
 });

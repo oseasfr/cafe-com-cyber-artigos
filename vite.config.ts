6dc7d57acd0c67e59097e0d7f66d8317350a21a0
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-02-05 22:25:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { plugin as markdown } from "vite-plugin-markdown"; // Adicione isso
+import markdown from "vite-plugin-md";
 
 export default defineConfig({
   base: "/cafe-com-cyber-artigos/",
   plugins: [
-    react(),
-    markdown({ mode: ["html", "react", "meta"] }) // Ativa o suporte a Markdown
+    react({
+      include: [/\.tsx$/, /\.md$/], // Faz o React entender o Markdown como componente
+    }),
+    markdown(),
   ],
   resolve: {
     alias: {

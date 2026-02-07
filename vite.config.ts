a3b25dc009e20a9f790ed62a35963cc4bd8ba510
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-02-06 21:27:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { plugin as markdown } from "vite-plugin-markdown";
-
 export default defineConfig({
-  base: "/cafe-com-cyber-artigos/",
   plugins: [
     react(),
-    markdown({ mode: ['react'] }) // Força o modo React para evitar erros de sintaxe JS
+    markdown({ mode: ['react'] })
   ],
   resolve: {
     alias: {

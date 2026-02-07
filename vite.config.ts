6d0c8eb1f6cd6aea20dc03885064d4d399fa199d
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-02-06 23:04:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-
-  // Permite que o Vite inclua arquivos .md no bundle
+  base: "/", // 🔥 ESSENCIAL
+  plugins: [react()],
   assetsInclude: ["**/*.md"],
-
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

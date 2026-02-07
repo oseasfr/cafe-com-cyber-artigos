64c205979e3e2fa874f020fe078c4e957806f9c6
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-02-06 22:40:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import MarkdownPreview from "vite-plugin-markdown-preview";
 
 export default defineConfig({
   plugins: [
-    react(),
-    MarkdownPreview()
+    react()
   ],
 
-  // 🔥 ESSENCIAL: permite que o Vite inclua arquivos .md no bundle
+  // Permite que o Vite inclua arquivos .md no bundle
   assetsInclude: ["**/*.md"],
 
   resolve: {

91ad10fb1ec6d4a2674e1228eaef9369232fec9c
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-02-06 23:16:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
 export default defineConfig({
-  root: "src",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-  },
   base: "/",
+  plugins: [react()],
+  assetsInclude: ["**/*.md"],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });

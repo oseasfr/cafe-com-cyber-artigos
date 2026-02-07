9007c083036ca3a4e839395481a299627e497c0f
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-02-06 21:34:33 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
 export default defineConfig({
   plugins: [
-    react(),
-    markdown({ mode: ['react'] })
+    react()
   ],
   resolve: {
     alias: {

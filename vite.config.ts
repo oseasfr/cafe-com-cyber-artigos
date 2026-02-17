74f4a3f9d217f1fcb42780fca6bcc55e59c06862
Oséas de Freitas Rosa <86753305+oseasfr@users.noreply.github.com>
2026-02-17 20:44:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,43 +1,21 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode } ) => {
-
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    build: {
-     
-      rollupOptions: {
-        output: {
-          manualChunks(id: string) {
-          
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          }
-        }
-      }
+export default defineConfig({
+  base: "/cafe-com-cyber-artigos/",
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: path.resolve(__dirname, "index.html"),
     },
-
-  
-    server: {
-      host: "::",
-      port: 8080,
-    },
-    plugins: [
-      react(),
-    
-      mode === 'development' && componentTagger(),
-    ].filter(Boolean),
-    resolve: {
-      alias: {
-   
-        "@": path.resolve(__dirname, "./src"),
-      },
-      extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json'],
-    },
-  };
+  },
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+    extensions: [".mjs", ".js", ".mts", ".ts", ".jsx", ".tsx", ".json"],
+  },
+  server: {
+    host: "::",
+    port: 8080,
+  },
 });
